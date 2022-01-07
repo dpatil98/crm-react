5d2559f0c7459e0c08bf2275d143c881e8ef48ce
Dhananjay Patil <dsp5898@gmail.com>
2022-01-07 14:02:34 +0530

    chage in global contraints

--- src/global-constants.js
+++ src/global-constants.js
@@ -1,2 +1,2 @@
-export const API_URL="http://localhost:9000";
-// export const API_URL="https://crm-hackathon2.herokuapp.com";
+// export const API_URL="http://localhost:9000";
+export const API_URL="https://crm-hackathon2.herokuapp.com";
